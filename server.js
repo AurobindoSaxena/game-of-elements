3ba11ca2d72ab88d3f2994c6aebab91dfb90b3ad
Aurobindo Saxena <88999345+AurobindoSaxena@users.noreply.github.com>
2025-04-18 22:40:59 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+const games = {};
 
 io.on('connection', socket => {
-  socket.on('createGame', ({ playerName, expectedPlayers }) => {
-    const gameId = uuidv4().slice(0, 6);
+  socket.on('createGame', ({ playerName, numPlayers }) => {
+    const gameId = uuid().slice(0, 6);
+    games[gameId] = { players: {}, numPlayers, choices: {} };
+    games[gameId].players[playerName] = socket.id;
     socket.join(gameId);
-    io.to(gameId).emit('gameCreated', { gameId, playerName, expectedPlayers });
+    socket.emit('gameCreated', { gameId });
+  });
+
+  socket.on('joinGame', ({ playerName, gameId }) => {
+    const game = games[gameId];
+    if (!game) return socket.emit('error', 'Invalid Game ID');
+    game.players[playerName] = socket.id;
+    socket.join(gameId);
+    socket.emit('gameJoined', { gameId });
+  });
+
+  socket.on('submitChoice', ({ gameId, playerName, element }) => {
+    const game = games[gameId];
+    game.choices[playerName] = element;
+    if (Object.keys(game.choices).length == game.numPlayers) {
+      const results = Object.entries(game.choices).map(([player, choice]) => `${player}: ${choice}`).join(', ');
+      io.in(gameId).emit('showResult', { message: `Choices - ${results}` });
+      game.choices = {};
+    }
   });
 });
 
-http.listen(process.env.PORT || 3000, () => {
-  console.log('Server running');
-});
+http.listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
